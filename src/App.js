5012a2ecb247d60b5b83f535e6c0a6ba5ecd30bb
kasidej02 <kasidej.kammool@gmail.com>
2021-02-09 14:12:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ import { useState, useEffect } from "react";
 function App() {
   //add useState for all state variables
 
+  const [name, setname] = useState('');
+  const [gender, setgender] = useState('');
+  const [age, setage] = useState('');
+
+  // console.log(name)
+  const onSubmit = (e) => {
+    const payload = {
+      name,
+      gender,
+      age
+    }
+    console.log('pl' , payload);
+    
+    <ItemTable name={name} gender={gender} age={age} />
+
+  }
+  
+
   //load locationStorage
   useEffect(() => {
     const items = localStorage.getItem("items");
     // ...
+    localStorage.setItem('items',JSON.stringify(items))
   }, []);
 
   return (
@@ -21,12 +40,13 @@ function App() {
             type="text"
             placeholder="e.q John Smith"
             //update related state based on event
+            onChange = {(e) => setname(e.target.value)}
           ></input>
         </div>
 
         <div className="field">
           <label className="label">Gender</label>
-          <select className="input" type="text" placeholder="Please select ..">
+          <select className="input" type="text" placeholder="Please select .." > onChange={(e)=>setgender(e.target.value)}
             <option value="" disabled selected hidden>
               -- Select Gender --
             </option>
@@ -37,10 +57,14 @@ function App() {
 
         <div className="field">
           <label className="label">Age</label>
-          <input className="input" type="number" placeholder="e.q 30"></input>
+          <input className="input" type="number" placeholder="e.q 30"
+          onChange = {(e)=>setage(e.target.value)}
+          ></input>
         </div>
 
-        <button className="button is-primary is-fullwidth">Submit</button>
+        <button className="button is-primary is-fullwidth"
+        onClick={onSubmit}
+        >Submit</button>
 
         <div className="mb-4"></div>
 
@@ -48,7 +72,8 @@ function App() {
         <p className="is-4 title has-text-centered">Person List</p>
         {/* sample table */}
         <ItemTable name={"Bob"} gender={"Male"} age={"50"} />
-        <p>Your name and code here</p>
+        {/* <ItemTable name={name} gender={gender} age={age} /> */}
+        <p>Kasidej Kammool #620610776</p>
       </div>
     </div>
   );
